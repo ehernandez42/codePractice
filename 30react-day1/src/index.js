520ca14fed8af87cde5bf3b6bee6aec875867a76
Eleazar <eleazarfhernandez@gmail.com>
2023-08-17 21:55:08 -0500

    commit

---
+++ 30react-day1/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsxElement = <h1>This is a JSX element</h1>
+const rootElement  = document.getElementById('root')
+const title = <h2>Getting Started React</h2>
+
+ReactDOM.render(jsxElement, rootElement)
+ReactDOM.render(title, rootElement)
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
