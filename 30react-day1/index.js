2532735c259651eec54c65f426ef75561691e08f
Eleazar Hernandez <80542211+ehernandez42@users.noreply.github.com>
2023-08-24 11:25:54 -0500

    Add files via upload
    index.js file for React day 4 - Components

--- 30react-day1/index.js
+++ 30react-day1/index.js
@@ -1,84 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CSSimg from "./images/css_logo.png";
-import HTMLimg from "./images/html_logo.png"
-import ReactImg from "./images/react_logo.png"
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
-const header = (
+const title = 'Hexacolor Generator'
+const Header = () => (
     <header>
-        <div className='header-wrapper'>
-        <h1>Welcome to 30 days of react</h1>
-        <h2>Getting Started React</h2>
-        <h3>Javascript Library</h3>
-        <p>Eleazar Hernandez</p>
+        <div className={'header-wrapper'}>
+            <h1>{title}</h1>
         </div>
     </header>
 )
 
-const main = (
-    <main>
-        <p>Pre-requisites to get started {' '}
-        <strong>
-            <em>react.js</em>
-        </strong>
-        </p>
-        <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-        </ul>
-    </main>
-)
+const buttonStyles = {
+    background: 'black',
+    color: 'white',
+}
 
-const footer = (
-    <footer>
-        <div className='footer-wrapper'>
-        <p>Copyright 2023</p>
-        </div>
-    </footer>
-)
+const Button = () => <button style={buttonStyles}> action </button>
 
-const technologies = (
-    <main>
-    <h2 className={"tech-title"}>Front End Technologies</h2>
-    <div className="technologies-section">
-        <img src={CSSimg}  alt={"css"}/>
-        <img src={HTMLimg} alt={"html"} />
-        <img src={ReactImg} alt="react img" />
-    </div>
-    </main>
-)
+const hexaColor = () => {
+    let str = '0123456789abcdef'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        let index = Math.floor(Math.random() * str.length)
+        color += str[index]
+    }
+    return '#' + color
+}
 
-const subscribe = (
-    <section className={"subscribe-section"}>
-        <h2 className={"tech-title"}>Subscribe</h2>
-        <p>Sign up with your email address to receive cool stuff in your inbox</p>
-        <div className={"input-area"}>
-        <input className={"subscribe-input"} placeholder={"First name"}/>
-            <input className={"subscribe-input"} placeholder={"Last name"}/>
-            <input className={"subscribe-input"} placeholder={"Email"}/>
-        </div>
-        <div className={"button-area"}>
-            <button className={"subscribe-button"}>Subscribe</button>
-        </div>
-    </section>
-)
 
-const app = (
+const divStyle = {
+    background: `${hexaColor()}`,
+    padding: '10px',
+    margin: '2px',
+    border: 'none',
+    textAlign: 'center',
+    fontFamily: 'sans-serif',
+    fontWeight: 'bold',
+    color: 'white',
+}
+
+const HexaColor = () => (
     <div>
-        {header}
-        {main}
-        {technologies}
-        {subscribe}
-        {footer}
+        <div style={divStyle}>
+            {hexaColor()}
+        </div>
+        <div style={divStyle}>
+            {hexaColor()}
+        </div>
     </div>
+    )
+//this is where I left off: trying to separate the different divs so that they
+//can have their own background colors
+const hexaSectionsFormatted = HexaColor.map((hexaColor) => <div>{HexaColor}</div>)
+
+const App = () => (
+    <div>
+        <Header />
+        <HexaColor />
+        <p>{hexaSectionsFormatted}</p>
+        <Button />
+    </div>
+
 )
 
-    const rootElement = document.getElementById('root')
-    ReactDOM.render(app, rootElement)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+const rootElement = document.getElementById('root')
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement)
