311075ad496ab0d4b3ebaee2aca2e8af7dad3012
Eleazar Hernandez <80542211+ehernandez42@users.noreply.github.com>
2023-10-06 10:03:27 -0500

    Practiced node with a hello world server file

---
+++ server.js
@@ -0,0 +1,16 @@
+//hello world practice
+
+/*const http = require('http');
+
+const hostName = '127.0.0.1';
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader = ('Content-type', 'text/plain');
+    res.end('Hello World');
+})
+
+server.listen(PORT, hostName, () => {
+    console.log(`server is listening on http://${hostName}:${PORT}/`)
+})*/
